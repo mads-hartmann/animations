fa5d17e99cbfe7c353a4342d3335e05270cefbd6
Mads Hartmann <mads379@gmail.com>
2024-05-14 20:31:59 +0000

    Looks a bit better now

--- src/animations/DynamicIsland.tsx
+++ src/animations/DynamicIsland.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
-import { X } from "lucide-react";
+import { PlusIcon, Settings, X } from "lucide-react";
 
 export default function DynamicIsland() {
   const [isOpen, setIsOpen] = useState(false);
@@ -8,23 +8,35 @@ export default function DynamicIsland() {
   const shared =
     "border-1 overflow-hidden rounded-[14px] border border-[#242524] text-sm text-white shadow-menu bg-[#161716] " +
     "flex justify-center items-center " +
-    "mx-auto w-24 " +
-    "fixed bottom-6 left-6 right-6 ";
+    "mx-auto " +
+    "fixed bottom-6 left-6 right-6 w-24";
 
   const btn = "cursor-pointer text-nowrap px-3 py-2 font-medium text-[#929292]";
 
+  const openVariants = {
+    hidden: { opacity: 0, width: 96, height: 66 },
+    visible: { opacity: 1, width: 600, height: 400 },
+  };
+
+  const closeVariants = {
+    hidden: { opacity: 0, width: 600, height: 400 },
+    visible: { opacity: 1, width: 96, height: 66 },
+  };
+
   return (
     <div>
-      {isOpen ? (
-        <AnimatePresence>
+      <AnimatePresence mode="wait">
+        {isOpen ? (
           <motion.div
+            layout
             layoutId="rectangle"
-            className={`${shared} w-[600px] h-[300px]`}
+            className={`${shared}`}
+            variants={openVariants}
+            initial="hidden"
+            animate="visible"
+            exit="hidden"
           >
             <motion.button
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
               layout
               onClick={() => setIsOpen(false)}
               className={btn + " absolute top-2 right-2"}
@@ -36,20 +48,35 @@ export default function DynamicIsland() {
               <motion.span>Open a repo in a new environment</motion.span>
             </motion.div>
           </motion.div>
-        </AnimatePresence>
-      ) : (
-        <AnimatePresence>
-          <motion.div layoutId="rectangle" className={`${shared} size-[100px]`}>
-            <motion.button
-              layout
-              onClick={() => setIsOpen(true)}
-              className={btn}
-            >
-              Open
-            </motion.button>
+        ) : (
+          <motion.div
+            layout
+            layoutId="rectangle"
+            className={`${shared} flex shrink items-end gap-2 p-2 shadow-lg`}
+            variants={closeVariants}
+            initial="hidden"
+            animate="visible"
+            exit="hidden"
+          >
+            <div className="flex place-content-center">
+              <motion.button
+                layout
+                onClick={() => setIsOpen(true)}
+                className={btn}
+              >
+                <PlusIcon />
+              </motion.button>
+              <motion.button
+                layout
+                onClick={() => setIsOpen(true)}
+                className={btn}
+              >
+                <Settings />
+              </motion.button>
+            </div>
           </motion.div>
-        </AnimatePresence>
-      )}
+        )}
+      </AnimatePresence>
     </div>
   );
 }
